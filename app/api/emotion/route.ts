108cb8cb4a3d8e3a29d703790ac671df4d8ec28d
sergiovalbuena <iosergio.valbuena@gmail.com>
2024-02-20 17:50:12 -0800

    Refactor emotion route to include text classification and filtering

--- app/api/emotion/route.ts
+++ app/api/emotion/route.ts
@@ -1,8 +1,41 @@
-import { HfInference } from "@huggingface/inference";
+import { HfInference, TextClassificationOutput } from "@huggingface/inference";
 
-const hf = new HfInference(process.env.HF_API_KEY);
+let hf: HfInference;
 
 export async function POST(req: Request, res: Response) {
     const { input } = await req.json();
-  return new Response("dummy response, {status: 200}");
+    const inferenceResponse: TextClassificationOutput = await runInference(
+      input
+    );
+    //console.log(inferenceResponse);
+    const filteredResponse = filterResponse([...inferenceResponse])
+  return new Response(JSON.stringify({inferenceResponse, filteredResponse}), {status: 200});
+}
+
+async function runInference(input: string) {
+    if(!hf){
+        hf = new HfInference(process.env.HF_API_KEY);
+    }
+
+    const modelName = 'SamLowe/roberta-base-go_emotions';
+    const inferenceRes = await hf.textClassification({
+        model: modelName,
+        inputs: input
+    })
+    return inferenceRes;
+}
+
+function filterResponse(emotions: TextClassificationOutput){
+    //loop over the emotions
+    const filtered = []
+    const emotion0 = emotions.shift();
+    filtered.push(emotion0);
+    let score = emotion0?.score;
+    for (let emotion of emotions){
+        if(emotion.score > score!){
+            filtered.push(emotion);
+            score = emotion.score;
+        }
+    }
+    return filtered;
 }
